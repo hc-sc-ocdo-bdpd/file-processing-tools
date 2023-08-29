15092eede226bd18cfb4f8ea07893bdb27b2607d
efung <erika.fung@hc-sc.gc.ca>
2023-08-29 14:18:43 -0400

    Update alt_Table_processor_main.py

--- table_processing/alt_Table_processor_main.py
+++ table_processing/alt_Table_processor_main.py
@@ -10,7 +10,7 @@ logging.basicConfig(filename='alt_table_detection.log', filemode='a', datefmt='%
 logging.getLogger().addHandler(logging.StreamHandler())
 
 def Average(lst):
-    return int(statistics.median(lst))
+    return math.ceil(sum(lst) / len(lst)) # int(statistics.median(lst))
 
 def get_tbl(txt):
     dir = os.path.dirname(txt)
@@ -19,17 +19,18 @@ def get_tbl(txt):
         for line in r:
             if len(line.strip()) > 5:
                 df+= line
-    tbls = df.strip().split("Table")
+    tbls = df.strip().split("Tab")
     for count, value in enumerate(tbls):
         output_txt = dir + '\output' + str(count) + '.txt'
         output_xl = dir + '\output' + str(count) + '.xlsx'
         with open(output_txt, "w") as txt_file:
-            txt_file.write(value)
+            txt_file.write(value.strip())
             txt_file.close()
         with open(output_txt, "r") as txt_file:
             num_words_total = []
             num_spaces_total = []
             for line in txt_file:
+                print(line.split())
                 num_spaces = len(line.split())
                 num_words = 0
                 for letter in line:
@@ -38,8 +39,10 @@ def get_tbl(txt):
                 num_spaces_total.append(num_spaces)
             txt_file.close()
         
-        num_col = Average(num_spaces_total) - 1
-        col_width = int(Average(num_words_total)/num_col)
+        num_col = Average(num_spaces_total) 
+        col_width = math.ceil((Average(num_words_total))/num_col)
+        print(num_col)
+
         col_specs = []
         start = 0
         end = col_width
@@ -48,9 +51,7 @@ def get_tbl(txt):
             col_specs.append(tup)
             start += col_width
             end += col_width
-        print(col_specs)
-        tbl = pd.read_fwf(output_txt, colspecs=col_specs)
-        print(tbl)
+        tbl = pd.read_fwf(output_txt, colspecs=col_specs, skipfooter=1)
         tbl.to_excel(output_xl, index=None)
 
     # read_file = pd.read_csv(txt, encoding= 'unicode_escape')
@@ -59,4 +60,6 @@ def get_tbl(txt):
     # read_file.to_excel(xl, index=None)
 
 get_tbl('C:\\Users\\EFUNG\\Downloads\\PMRA TABLES_Redacted (provided by Martin L).txt')
+# get_tbl('C:\\Users\\EFUNG\\Downloads\\PMRA Table 7.txt')
+
 
