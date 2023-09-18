7832fc670bc6b10e7b1c2c740b261ed2149461c5
efung <erika.fung@hc-sc.gc.ca>
2023-09-18 11:54:27 -0400

    extract tables

--- file_processing/docx_processor.py
+++ file_processing/docx_processor.py
@@ -1,6 +1,6 @@
 from file_processor_strategy import FileProcessorStrategy
 from docx import Document
-
+import pandas as pd
 class DocxFileProcessor(FileProcessorStrategy):
     def __init__(self, file_path: str) -> None:
         super().__init__(file_path)
@@ -22,6 +22,23 @@ class DocxFileProcessor(FileProcessorStrategy):
             for para in doc.paragraphs:
                 full_text.append(para.text)
             return '\n'.join(full_text)
+        except Exception as e:
+            print(f"Error encountered while opening or processing {file_path}: {e}")
+            return None
+
+    def extract_tbls_from_docx(doc: Document) -> str:
+        try:
+            writer = pd.ExcelWriter(file_path.rsplit('.', 1)[0].lower()+'_tbls.xlsx', engine='xlsxwriter', engine_kwargs={'options': {'encoding': 'utf8'}})
+            # for each table in doc
+            for count, table in enumerate(doc.tables):
+                df = pd.DataFrame()
+                # write rows of table to df
+                for row in table.rows:
+                    text = [cell.text for cell in row.cells]
+                    df = pd.concat([df, pd.DataFrame([text])], ignore_index=True)
+                # write df to excel file
+                df.to_excel(writer, sheet_name=file_path.rsplit('.', 1)[0].lower()[:20]+"_tbl_"+str(count))
+            writer.close()
         except Exception as e:
             print(f"Error encountered while opening or processing {file_path}: {e}")
             return None
--- file_processing/pdf_processor.py
+++ file_processing/pdf_processor.py
@@ -1,5 +1,8 @@
 from file_processor_strategy import FileProcessorStrategy
 from PyPDF2 import PdfReader
+import os
+import pandas as pd
+from pdf2docx import Converter
 
 class PdfFileProcessor(FileProcessorStrategy):
     def __init__(self, file_path: str) -> None:
@@ -21,3 +24,20 @@ class PdfFileProcessor(FileProcessorStrategy):
         except Exception as e:
             print(f"Error encountered while opening or processing {file_path}: {e}")
             return None
+
+    def extract_tbls_from_pdf(file_path: str) -> str:
+        doc_path = file_path.rsplit('.', 1)[0].lower() + ".docx"
+        head, tail = os.path.split(file_path)
+        doc = head.rsplit('.', 1)[0].lower()
+        writer = pd.ExcelWriter(file_path.rsplit('.', 1)[0].lower()+'_tbls.xlsx', engine='xlsxwriter', engine_kwargs={'options': {'encoding': 'utf8'}})
+        try:
+            cv = Converter(file_path)
+            tables = cv.extract_tables(start=0, end=None)
+            cv.close()
+            for count, table in enumerate(tables):
+                df = pd.DataFrame(table)
+                df = df.apply(lambda x: [r.encode("utf8").split(b";base64,")[0].replace(b'\xef\xbf\xbd',b' ').decode("utf8") for r in x])
+                df.to_excel(writer, sheet_name=doc[:20]+"_tbl_"+str(count))
+            writer.close()
+        except Exception as e:
+            print(f"Error encountered while opening or processing {file_path}: {e}")
