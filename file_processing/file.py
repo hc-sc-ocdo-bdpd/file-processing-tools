eaa8455ffdc7f15b5448ac955215c26d6bdc5480
Brennan Cruse <brennan.cruse@hc-sc.gc.ca>
2023-11-22 09:12:29 -0500

    file

--- file_processing/file.py
+++ file_processing/file.py
@@ -1,22 +1,22 @@
 from pathlib import Path
-from txt_processor import TextFileProcessor
-from pdf_processor import PdfFileProcessor
-from docx_processor import DocxFileProcessor
-from ocr_decorator import OCRDecorator
-from msg_processor import MsgFileProcessor
-from png_processor import PngFileProcessor
-from xlsx_processor import xlsxFileProcessor
-from pptx_processor import PptxFileProcessor
-from rtf_processor import RtfFileProcessor
-from html_processor import HtmlFileProcessor
-from xml_processor import XmlFileProcessor
-from jpeg_processor import JpegFileProcessor
-from csv_processor import CsvFileProcessor
-from json_processor import JsonFileProcessor
-from zip_processor import ZipFileProcessor
-from generic_processor import GenericFileProcessor
-from py_processor import PyFileProcessor
-from errors import UnsupportedFileTypeError, NotOCRApplciableError
+from .txt_processor import TextFileProcessor
+from .pdf_processor import PdfFileProcessor
+from .docx_processor import DocxFileProcessor
+from .ocr_decorator import OCRDecorator
+from .msg_processor import MsgFileProcessor
+from .png_processor import PngFileProcessor
+from .xlsx_processor import xlsxFileProcessor
+from .pptx_processor import PptxFileProcessor
+from .rtf_processor import RtfFileProcessor
+from .html_processor import HtmlFileProcessor
+from .xml_processor import XmlFileProcessor
+from .jpeg_processor import JpegFileProcessor
+from .csv_processor import CsvFileProcessor
+from .json_processor import JsonFileProcessor
+from .zip_processor import ZipFileProcessor
+from .generic_processor import GenericFileProcessor
+from .py_processor import PyFileProcessor
+from .errors import UnsupportedFileTypeError, NotOCRApplciableError
 
 class File:
     OCR_APPLICABLE_EXTENSIONS = {".pdf", ".jpeg", ".png"}
